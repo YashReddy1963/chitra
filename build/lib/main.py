a8dfd2fcd49caaabaf8ba7148ee2e4fa284ee209
mangeshrex <mangeshrex@tutanota.com>
2024-07-27 04:11:30 +0530

    fix(ruff): fix ruff formatting

--- build/lib/main.py
+++ build/lib/main.py
@@ -1,7 +1,9 @@
 import sys 
+import os
 import gi 
-from gi.repository import GLib, Gtk 
+
 gi.require_version("Gtk", "4.0")
+from gi.repository import GLib, Gtk 
 
 class Chitra(Gtk.Application):
     def __init__(self):
