9af0217b083df1e7246a9fecf115048a24a34010
mangeshrex <mangeshrex@tutanota.com>
2024-07-27 12:33:40 +0530

    feat: adding text and image to the GTK window

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import sys 
 import gi 
 gi.require_version("Gtk", "4.0")
-from gi.repository import GLib, Gtk 
+from gi.repository import GLib, Gtk
 
 class Chitra(Gtk.Application):
     # Post screenshot customization 
-    def customizeScreennshot(self):
-        super().customizeScreenshot(application_id="com.chitra.postScreenshot") 
+    def customizeScreenshot(self):
+        super().customizeScreenshot(title="CHitra") 
 
-        box = self.get_content_area()
-        label = Gtk.Label(label="Chitra -- customize your screenshot")
-        box.add(label)
+        header_bar = Gtk.HeaderBar()
+        header_bar.set_show_close_button(True)
+        header_bar.props.title = "Chitra - customize your screenshot"
+        self.set_titlebar(header_bar)
+        self.set_default_size(500, 400)
+
+        # Textview 
+        self.text_buffer = Gtk.TextBuffer()
+        self.textView = Gtk.TextView(buffer=self.text_buffer)
+        self.textView.set_wrap_mode(Gtk.WrapMode.WORD)
+        self.text_buffer = self.textview.get_buffer()
+        self.entry = Gtk.Entry("Chitra")
+        self.entry.set_text("Chitra")
+
 
         GLib.set_application_name("Chitra")
 
     def do_activate(self):
         window = Gtk.ApplicationWindow(application=self, title="Chitra")
+                # Adding a label heading to the app
+        label = Gtk.Label(label="Chitra", xalign=20, yalign=20)
+        label.set_markup("<span font='20' weight='bold'>Chitra</span> ") 
+
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL,spacing=5)
+                # Adding Picture Preview 
+        picture = Gtk.Picture.new_for_filename('../../test-neofetch.png')
+        picture.set_size_request(100, 200)
+
+        hbox.append(label)
+        hbox.append(picture)
+        window.set_child(hbox)
+
+        print(hbox)
         window.present()
 
 app = Chitra()
